a7cb2cf8b473189c64a6ddb298a0eba44b6f47ed
Brian Clark <brian.w.clark176@gmail.com>
2021-08-21 17:32:31 -0600

    Initial commit

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+
 mongoose.connect("mongodb://localhost/petdb", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
 })
     .then(() => console.log("Established a connection to MongoDB"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
